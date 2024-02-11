2aa33080062e88cce86a9fe5a60c7e72efeb6ed0
Basma Alaa Mahmoud <121583270+basmaAlaaElSayed@users.noreply.github.com>
2024-02-12 00:13:43 +0200

    First Day of Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Defines the route for the "blog_posts" resource
+  resources :blog_posts
+  root "blog_posts#index"
+#get "up" => "rails/health#show", as: :rails_health_check
+#patch "/blog_posts/:id" => "blog_posts#update", as: :update_blog_post
+#delete "/blog_posts/:id" => "blog_posts#destroy" , as: :delete_blog_post
+#get "/blog_posts/new" => "blog_posts#new", as: :new_blog_post
+#get "/blog_posts/:id" => "blog_posts#show" , as: :blog_post # when you go to url /blog_posts/id it will call the show method in the blog_posts controller
+#post "/blog_posts" => "blog_posts#create", as: :blog_posts
+#get "/edit_blog_post/:id" => "blog_posts#edit", as: :edit_blog_post
 end
