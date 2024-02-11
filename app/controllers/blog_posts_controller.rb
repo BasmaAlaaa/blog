1d67e0bd29300e7f3637cd6c565960d7f469257d
Basma Alaa Mahmoud <121583270+basmaAlaaElSayed@users.noreply.github.com>
2024-02-12 00:15:44 +0200

    first day

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,10 +1,12 @@
 class BlogPostsController < ApplicationController
+before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+
     def index
         @blog_posts = BlogPost.all
     end
 
     def show
-        @blog_post = BlogPost.find(params[:id])
+     #   @blog_post = BlogPost.find(params[:id])
         #when you enter an invalid id, it will redirect to the root path
     rescue ActiveRecord::RecordNotFound
         redirect_to root_path
@@ -24,11 +26,11 @@ class BlogPostsController < ApplicationController
     end
 
     def edit
-        @blog_post = BlogPost.find(params[:id])
+      #  @blog_post = BlogPost.find(params[:id])
     end
 
     def update
-        @blog_post = BlogPost.find(params[:id])
+     #   @blog_post = BlogPost.find(params[:id])
         if @blog_post.update(blog_post_params)
             redirect_to blog_post_path(@blog_post)
         else
@@ -37,12 +39,15 @@ class BlogPostsController < ApplicationController
     end
 
     def destroy
-        @blog_post = BlogPost.find(params[:id])
+      #  @blog_post = BlogPost.find(params[:id])
         @blog_post.destroy
         redirect_to root_path, notice: 'Blog post was successfully deleted.'
       end
     private
     def blog_post_params
         params.require(:blog_post).permit(:title, :body)
+    end
+    def set_blog_post
+        @blog_post = BlogPost.find(params[:id])
     end
 end
